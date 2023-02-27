01da3a4ca5085d05ff77bd2c3601bdc2cda1a70e
Zireael-3 <vladgnatenko203@gmail.com>
2023-02-27 17:29:32 +1000

    Add file

---
+++ hw1/task05.py
@@ -0,0 +1,28 @@
+"""
+Given a list of integers numbers "nums".
+
+You need to find a sub-array with length less equal to "k", with maximal sum.
+
+The written function should return the sum of this sub-array.
+
+Examples:
+    nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+    result = 16
+"""
+from typing import List
+
+
+def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
+    max_sum = 0
+    for i in range(len(nums)):
+        curr_sum = 0
+        for j in range(len(nums)):
+            if i - j < k:
+                curr_sum += nums[j]
+        max_sum = curr_sum
+    return max_sum
+
+
+n = [1, 3, -1, -3, 5, 3, 6, 7]
+r = find_maximal_subarray_sum(n, 3)
+print(r)
