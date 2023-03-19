a493c2bf7461f7ad6cf94b6b7f6e20c02138ec06
Zireael-3 <vladgnatenko203@gmail.com>
2023-03-19 20:26:44 +1000

    Edited version of function 1

--- hw2/task01.py
+++ hw2/task01.py
@@ -6,22 +6,29 @@ Given a file containing text. Complete using only default collections:
     4) Count every non ascii char
     5) Find most common non ascii char for document
 """
-from typing import List
 import string
+from typing import List
+import re
 
 
 def get_longest_diverse_words(file_path: str) -> List[str]:
+    """function that finds 10 longest words consisting from largest amount of unique symbols"""
     with open(file_path) as file:
         text = file.read().encode().decode('unicode-escape')
         text = text.replace('\n', '')
+        text = re.sub('[.№;%:?*()_+=/!@#$]', ' ', text)
+        text = re.sub(r'\d+', ' ', text)
+
         words = text.split(' ')
         list_of_l = []
         i = 0
         while i < len(words):
+            """checkin if a word consists of unique characters"""
             if len(set(words[i])) == len(words[i]):
                 list_of_l.append(words[i])
             i += 1
         sorted_list = sorted(list_of_l, key=len)
+        """returning the result"""
         result = []
         j = 10
         while j > 0:
@@ -30,10 +37,8 @@ def get_longest_diverse_words(file_path: str) -> List[str]:
         return result
 
 
-print(get_longest_diverse_words('data.txt'))
-
-
 def get_rarest_char(file_path: str) -> str:
+    """function that finds the rarest symbol for document using dictionary"""
     with open(file_path) as file:
         text = file.read().encode().decode('unicode-escape')
         text = text.replace('\n', '')
@@ -47,10 +52,8 @@ def get_rarest_char(file_path: str) -> str:
         return rarest
 
 
-# print((get_rarest_char("data.txt")))
-
-
 def count_punctuation_chars(file_path: str) -> int:
+    """function that counts every punctuation char by the discarding all other kinds of characters"""
     with open(file_path) as file:
         text = file.read().encode().decode('unicode-escape')
         text = text.replace('\n', '')
@@ -66,9 +69,8 @@ def count_punctuation_chars(file_path: str) -> int:
         return count
 
 
-# print(count_punctuation_chars("data.txt"))
-
 def count_non_ascii_chars(file_path: str) -> int:
+    """function that counts every non ascii char"""
     with open(file_path) as file:
         text = file.read().encode().decode('unicode-escape')
         text = text.replace('\n', '')
@@ -77,12 +79,8 @@ def count_non_ascii_chars(file_path: str) -> int:
         return total_count - ascii_count
 
 
-# print((count_non_ascii_chars("data.txt")))
-
 def get_most_common_non_ascii_char(file_path: str) -> str:
-    """The `string.printable` constant contains all ASCII characters that can be printed,
-    which includes the digits, the letters (both lower and upper case),
-    punctuation marks, and whitespace characters."""
+    """function that finds most common non ascii char for document"""
     with open(file_path) as file:
         text = file.read().encode().decode('unicode-escape')
         text = text.replace('\n', '')
@@ -96,6 +94,3 @@ def get_most_common_non_ascii_char(file_path: str) -> str:
                 frequency[char] += 1
         rarest = max(frequency, key=frequency.get)
         return rarest
-
-
-# print(get_most_common_non_ascii_char("data.txt"))
