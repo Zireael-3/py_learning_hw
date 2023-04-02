4f92bb46d3eb9fb78720fc5c05d260ebf8520644
Zireael <115121474+Zireael-3@users.noreply.github.com>
2023-04-02 18:56:06 +1000

    Delete hw2 directory

--- hw2/hw2_tests/test_task05.py
+++
@@ -1,8 +0,0 @@
-from hw2.hw2_tasks.task05 import custom_range
-import string
-
-
-def test_custom_range():
-    assert custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 'f']
-    assert custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-    assert custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
